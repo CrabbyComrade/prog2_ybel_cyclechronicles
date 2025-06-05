e42ff70bd6ccd9be42a90e7eaf79435c01d0f9a0
CrabbyComrade <184875117+CrabbyComrade@users.noreply.github.com>
2025-06-05 19:43:22 +0200

    Implement test for deliver() and repair()

--- src/test/java/cyclechronicles/ShopTest.java
+++ src/test/java/cyclechronicles/ShopTest.java
@@ -1,6 +1,8 @@
 package cyclechronicles;
 
 import org.mockito.*;
+
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,7 +20,7 @@ public class ShopTest {
 
     @BeforeEach
     void setUp() {
-        MockitoAnnotations.openMocks(this);
+        orderMock = mock(Order.class);
         shop = new Shop();
     }
 
@@ -94,5 +96,67 @@ public class ShopTest {
         when(orderMock.getCustomer()).thenReturn("Kunde");
 
         assertTrue(shop.accept(orderMock), "Bestellung mit E-Bike sollte nicht akzeptiert werden");
+    }
+
+    // Tests for Task 2
+
+    @Test
+    void testRepair() {
+        Shop shopMock = mock(Shop.class);
+        Queue<Order> mockedPendingOrders = mock(Queue.class);
+        Set<Order> mockedCompletedOrders = mock(Set.class);
+
+        when(mockedPendingOrders.poll()).thenReturn(orderMock); // Return our orderMock when trying to take oldest order
+
+        doAnswer(x -> { // simulated behaviour of repair()
+            Order orderToRepair = mockedPendingOrders.poll(); // Retrieve and remove oldest element
+            mockedCompletedOrders.add(orderToRepair);  // put element into completedorders
+            return Optional.of(orderToRepair);
+        }).when(shopMock).repair();
+
+        Optional<Order> repairedOrder = shopMock.repair(); // Do mock repair
+        assertEquals(orderMock, repairedOrder.get()); // Checks if repair was successfull
+
+        verify(mockedCompletedOrders).add(orderMock); // Check if orderMock has been added to the completed orders
+    }
+
+    @Test
+    void testDeliver() {
+        // Mocked orders
+        Order existingOrder1 = mock(Order.class);
+        when(existingOrder1.getCustomer()).thenReturn("Kunde1");
+        Order existingOrder2 = mock(Order.class);
+        when(existingOrder2.getCustomer()).thenReturn("Kunde2");
+        Order existingOrder3 = mock(Order.class);
+        when(existingOrder3.getCustomer()).thenReturn("Kunde3");
+
+         // Mock a real Set of orders
+        List mockedCompletedOrders = new ArrayList<>(); // probably bad variable name bcz it's not a mock
+        mockedCompletedOrders.add(existingOrder1);
+        mockedCompletedOrders.add(existingOrder2);
+        mockedCompletedOrders.add(existingOrder3);
+
+        Shop spyShop = spy(shop);
+        doAnswer(x -> {
+            String customer = x.getArgument(0);  // Get customer from given String
+            Iterator<Order> iterator = mockedCompletedOrders.iterator(); // Make iterator for mocked orders
+            while (iterator.hasNext()) {
+                Order order = iterator.next();
+                if (order.getCustomer().equals(customer)) { // Check for whose order should be delivered
+                    iterator.remove(); // Remove order from mockedCompletedOrders
+                    return Optional.of(order);  // Returns finished order
+                }
+            }
+            return Optional.empty();  // returns empty optional if no orders are found
+        }).when(spyShop).deliver(anyString());
+
+        Optional<Order> deliveredOrder = spyShop.deliver(existingOrder2.getCustomer()); // Do mock deliver()
+        assertEquals(existingOrder2, deliveredOrder.get()); // Check right order has been delivered
+
+        assertTrue(mockedCompletedOrders.size()==2, "Es sollte zwei copmleted orders nach deliver() geben."); // Check if delivered order has been removed
+
+        // Test for nonexistent customer
+        deliveredOrder = spyShop.deliver("X");
+        assertFalse(deliveredOrder.isPresent(), "Deliver sollte empty zurückgeben");
     }
 }
